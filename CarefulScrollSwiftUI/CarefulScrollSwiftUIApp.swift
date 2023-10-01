b1a28c8f3d8640c07af94b0336b9cfe13eeca9c1
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-01 12:14:40 +0300

    Initial Commit

---
+++ CarefulScrollSwiftUI/CarefulScrollSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulScrollSwiftUIApp.swift
+//  CarefulScrollSwiftUI
+//
+//  Created by Николай Игнатов on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarefulScrollSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
